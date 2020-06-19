f406e8897a6e4783456674460c3df431c46bc8f9
Bluesman <dmitry.khalimov@gmail.com>
2020-06-19 11:41:25 +0300

    Добавлено перемещение диалогового окна

--- js/dialog.js
+++ js/dialog.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var SETUP_DEFAULT_X = '813.5px';
+  var SETUP_DEFAULT_Y = '80px';
 
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
@@ -23,6 +25,8 @@
   var closePopup = function () {
     setup.classList.add('hidden');
     document.removeEventListener('keydown', onPopupEscPress);
+    setup.style.left = SETUP_DEFAULT_X;
+    setup.style.top = SETUP_DEFAULT_Y;
   };
 
   setupOpen.addEventListener('click', function (evt) {
@@ -39,5 +43,56 @@
   setupClose.addEventListener('click', function (evt) {
     evt.preventDefault();
     closePopup();
+  });
+
+  var dialogHandle = setup.querySelector('.upload');
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandle.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandle.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
